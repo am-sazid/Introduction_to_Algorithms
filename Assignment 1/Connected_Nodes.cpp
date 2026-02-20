cde7b5e88df9686144c6fe6a6c774f85cc9cd985
am-sazid <amsazid.cse@gmail.com>
2026-02-20 12:21:37 +0600

    AssignmenT

---
+++ Assignment 1/Connected_Nodes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    int a[n];
+
+    vector<int> adj_list[n];
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    int q;
+    cin>>q;
+
+    while (q--)
+    {
+        int x;
+        cin>>x;
+
+        if(adj_list[x].empty()){
+            cout<<-1<<endl;
+        }
+        else{
+            sort(adj_list[x].begin(),adj_list[x].end(),greater<int>());
+            for (int m : adj_list[x])
+            {
+                cout<<m<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    
+
+    return 0;
+}
